dda8ca2d22fedcc4a2e280c20c962b8c15f71d6a
陳謙 <t112598078@ntut.edu.tw>
2023-12-28 14:55:15 +0800

    test: Add unit tests for letterGrade with boundary value analysis testing technique
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

--- src/test/java/TestLetterGrade.java
+++ src/test/java/TestLetterGrade.java
@@ -11,8 +11,23 @@ public class TestLetterGrade {
     }
 
     @Test
+    public void TestLetterGradeWithScore0GetF() {
+        Assert.assertEquals('F', letterGrade(0));
+    }
+
+    @Test
     public void TestLetterGradeWithScore30GetF() {
         Assert.assertEquals('F', letterGrade(30));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore59GetF() {
+        Assert.assertEquals('F', letterGrade(59));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore60GetD() {
+        Assert.assertEquals('D', letterGrade(60));
     }
 
     @Test
@@ -21,8 +36,28 @@ public class TestLetterGrade {
     }
 
     @Test
+    public void TestLetterGradeWithScore69GetD() {
+        Assert.assertEquals('D', letterGrade(69));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore70GetC() {
+        Assert.assertEquals('C', letterGrade(70));
+    }
+
+    @Test
     public void TestLetterGradeWithScore75GetC() {
         Assert.assertEquals('C', letterGrade(75));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore79GetC() {
+        Assert.assertEquals('C', letterGrade(79));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore80GetB() {
+        Assert.assertEquals('B', letterGrade(80));
     }
 
     @Test
@@ -31,8 +66,23 @@ public class TestLetterGrade {
     }
 
     @Test
+    public void TestLetterGradeWithScore89GetB() {
+        Assert.assertEquals('B', letterGrade(89));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore90GetA() {
+        Assert.assertEquals('A', letterGrade(90));
+    }
+
+    @Test
     public void TestLetterGradeWithScore95GetA() {
         Assert.assertEquals('A', letterGrade(95));
+    }
+
+    @Test
+    public void TestLetterGradeWithScore100GetA() {
+        Assert.assertEquals('A', letterGrade(100));
     }
 
     @Test
